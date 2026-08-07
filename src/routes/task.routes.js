d7b2ec50a0130843c0663ce423a1462987b4683a
alejandroiv <alejandroiv177771@gmail.com>
2026-08-07 12:49:24 -0300

    feat: se termino los controllers de task, faltan testearlo

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
+import { newTask, getTasks,getOneTask } from "../controllers/task.controller.js";
+
 
 const taskRoutes = Router();
 
 
 taskRoutes.post("/tasks", newTask)//añadir una nueva tarea
-taskRoutes.get("/tasks")//obtener todas las tareas
-taskRoutes.get("/tasks/:id")//obtener una tarea por su id
+taskRoutes.get("/tasks", getTasks)//obtener todas las tareas
+taskRoutes.get("/tasks/:id", getOneTask)//obtener una tarea por su id
 taskRoutes.put("/task/:id")//actualizar una tarea por su id
 taskRoutes.delete("/task/:id")//eliminar tarea por id
 
